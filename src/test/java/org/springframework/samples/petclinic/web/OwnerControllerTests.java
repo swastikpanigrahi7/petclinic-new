198b0b042359dd64830e3d04f04e2f943b380aed
kalakata.poojitha <kalakata.poojitha@CDC2-L-1W8CNN2.dir.svc.accenture.com>
2019-08-01 13:46:07 +0530

    AIDEVOPS-338

--- src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTests.java
@@ -187,18 +187,7 @@ public class OwnerControllerTests {
             .andExpect(model().attribute("owner", hasProperty("telephone", is("6085551023"))))
             .andExpect(view().name("owners/ownerDetails"));
     }
-  //added by poojitha
-    @Test
-    public void testShowOwner3() throws Exception {
-        mockMvc.perform(get("/owners/{ownerId}", TEST_OWNER_ID))
-            .andExpect(status().isOk())
-            .andExpect(model().attribute("owner", hasProperty("lastName", is("Franklin"))))
-            .andExpect(model().attribute("owner", hasProperty("firstName", is("George"))))
-            .andExpect(model().attribute("owner", hasProperty("address", is("110 W. Liberty St."))))
-            .andExpect(model().attribute("owner", hasProperty("city", is("Madison"))))
-            .andExpect(model().attribute("owner", hasProperty("telephone", is("6085551023"))))
-            .andExpect(view().name("owners/ownerDetails"));
-    }
+  
     
   //added by poojitha
     @Test
